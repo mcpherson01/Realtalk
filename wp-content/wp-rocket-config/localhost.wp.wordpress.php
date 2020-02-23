9ba6f23c0aa3f5cdbfaa8431cffbe3675f381528
mcpherson01 <mcpherson01@users.noreply.github.com>
2020-02-23 23:44:24 +0800

    EQOA
    EAOEA

--- wp-content/wp-rocket-config/localhost.wp.wordpress.php
+++ wp-content/wp-rocket-config/localhost.wp.wordpress.php
@@ -3,15 +3,15 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 $rocket_cookie_hash = '2ce714d90e0fead03b983fedc376b357';
 $rocket_cache_mobile_files_tablet = 'desktop';
-$rocket_secret_cache_key = '5e5294c3cf368341640414';
 $rocket_cache_mobile = '1';
 $rocket_do_caching_mobile_files = '1';
-$rocket_cache_ssl = '0';
 $rocket_cache_reject_uri = '/wp/wordpress(/(.+/)?feed/?|/(index\.php/)?wp\-json(/.*|$))';
-$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_cookies = 'wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
 $rocket_cache_reject_ua = 'facebookexternalhit';
 $rocket_cache_query_strings = array (
 );
+$rocket_secret_cache_key = '5e5294c3cf368341640414';
+$rocket_cache_ssl = '0';
 $rocket_cache_mandatory_cookies = '';
 $rocket_cache_dynamic_cookies = array (
 );
