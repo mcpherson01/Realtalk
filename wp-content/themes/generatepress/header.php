53e656ff0c8b05da60934a6e6ea579ed2f7916f6
mcpherson01 <mcpherson01@users.noreply.github.com>
2020-02-26 01:10:25 +0800

    Q;Q;
    Q;Q

--- wp-content/themes/generatepress/header.php
+++ wp-content/themes/generatepress/header.php
@@ -12,8 +12,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+
+<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta name="title" content="Realtalk Digital; The Home of RealtalkGames Youtube Explainer">
+	<meta name="description" content="Realtalk Digital; The Home of RealtalkGames Youtube Explainer.https://realtalk.digital,  facebook.com/realtalkgames, twitter.com/7GiveMeTheWorld">
+	<meta name="keywords" content="Half-life Alyx, Half-life Alyx Gordon Freeman, Half-life Alyx Gordon Freeman Gabe Newell, Half-life Alyx Gordon Freeman Gabe Newell Valve, Half-life Alyx Gordon Freeman Valve Gabe Newell, Half-life Alyx Gabe NewellHalf-life Alyx Gabe Newell Gordon Freeman, Half-life Alyx Gabe Newell Gordon Freeman Valve, Half-life Alyx Gabe Newell ValveHalf-life Alyx Gabe Newell Valve Gordon Freeman, Half-life Alyx ValveHalf-life Alyx Valve Gordon Freeman, Half-life Alyx Valve Gordon Freeman Gabe Newell,Half-life Alyx Valve, Half-life Alyx Valve Gabe Newell,Half-life Alyx Valve Gabe Newell Gordon FreemanHalf-life Gordon Freeman,Half-life Gordon Freeman Alyx,Half-life Gordon Freeman Alyx Gabe Newell,Half-life Gordon Freeman Alyx Gabe Newell Valve,Half-life Gordon Freeman,Half-life Gordon Freeman Alyx,realtalk, gaming, explanations,youtube,bioshock,fallout,steam,video,playthrough,realitymasher,james,mcpherson,simple,what,happened,games,videogaming,streaming,funnf,vids,narrator,vimeo,google,wiki,haly-life,steam,valve,official,trailer,content,footage,gameplay,explained,outlast,new,cool,funny,best">
+	<meta name="robots" content="index, follow">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="English">
+	<meta name="revisit-after" content="1 days">	
+	<meta name="author" content="James McPherson">
+	
+	<script type="application/javascript">var googletag=googletag||{};googletag.cmd=googletag.cmd||[];googletag.cmd.push(function(){googletag.pubads().disableInitialLoad()});</script><script type="application/javascript" src="//ap.lijit.com/www/headerauction/headersuite.min.js?configId=5567"></script>
+	<script src="//ap.lijit.com/www/delivery/fpi.js?z=688073&width=160&height=600"></script> 
+	<script src="//ap.lijit.com/www/delivery/fpi.js?z=688839&width=320&height=50"></script> 
+
 	<?php wp_head(); ?>
 </head>
 
